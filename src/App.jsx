1e8b9b4817e8c6ea69f2ec3fbfdd1ba268bd5f12
Ryotruuser <ryonantruuser@gmail.com>
2025-09-30 10:41:59 -0300

    first component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import './App.css'
 
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento(){
+  return(
+    <form className='form-evento'>
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">Preencha para criar um evento:</label>
+        <input type="text" id='nome'/>
+
+        <label htmlFor="data">Data do evento:</label>
+        <input type="date" id='data'/>
+
+        <label htmlFor="tema">Tema do evento:</label>
+        <input type="text" id='tema'/>
+      </fieldset>
+    </form>   
+  )
+}
+
 function App() {
   return (
     <main>
@@ -9,6 +29,7 @@ function App() {
       <section>
         <img src="/banner.png" alt="banner do tecboard" />
       </section>
+      <FormularioDeEvento/>
     </main>
 
   )
