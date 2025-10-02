317e2175dff9debd10a5dfc796b5a866789fc1c2
Ryotruuser <ryonantruuser@gmail.com>
2025-10-02 10:59:31 -0300

    estrutura do projeto e organização

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
 import './App.css'
+import { FormularioDeEvento } from './componentes/FormularioDeEvento'
 
 // no react, componentes são FUNÇÕES
-
-function TituloFormulario ({children}){
-  return (
-    <h2>{children}</h2>
-  )
-}
-
-function CampoDeFormulario({children}){
-  return (
-    <fieldset>
-      {children}
-    </fieldset>
-  )
-}
-
-function Label({children, htmlFor}){
-  return (
-    <label htmlFor={htmlFor}>
-      {children}
-    </label>
-  )
-}
-
-function CampoDeEntrada(props){
-  return (
-    <input {...props} />
-  )
-}
-
-function FormularioDeEvento(){
-  return(
-    <form className='form-evento'>
-      <TituloFormulario>
-        Preencha para criar um evento:
-      </TituloFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor="nome">Preencha para criar um evento:</Label>
-        <CampoDeEntrada 
-          type="text" 
-          id='nome' 
-          placeholder='Summer dev hits'
-          name='nomeEvento'
-        />
-      </CampoDeFormulario>
-    </form>   
-  )
-}
 
 function App() {
   return (
